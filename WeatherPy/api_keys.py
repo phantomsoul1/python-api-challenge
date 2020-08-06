3fb00c23b4a9d1756678d72e07af60f841ecdb36
George Hyros <65777363+phantomsoul1@users.noreply.github.com>
2020-08-06 09:02:59 -0400

    Revert "Merge branch 'master' of https://github.com/phantomsoul1/python-api-challenge"
    This reverts commit 7fb7b74ce1335a5ae10052d73b9c3b1c7502d4b3, reversing
    changes made to 6511e1d58b9bcc3da7ff6de3d402ed9ef0017eda.

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,12 +1,5 @@
 # OpenWeatherMap API Key
-<<<<<<< HEAD
 weather_api_key = "d19cd4f0355249b55eaa56b758b8bd5a"
 
 # Google API Key
 g_key = "AIzaSyAEUi6ucqXxVR9W1zV3xp9ClEXGMyxL4-4"
-=======
-weather_api_key = "YOUR KEY HERE!"
-
-# Google API Key
-g_key = "YOUR KEY HERE!"
->>>>>>> c5a06b83495205503fb073237c64bf4890439a6d
