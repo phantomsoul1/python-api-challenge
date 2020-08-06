1d6000b6d80df9077cbb159c9d7d53870cfc823e
George Hyros <65777363+phantomsoul1@users.noreply.github.com>
2020-08-06 08:51:14 -0400

    Misc

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d19cd4f0355249b55eaa56b758b8bd5a"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyC76xZrRDZaKS8NPrVNQOjec0iaEoY-8j8"
